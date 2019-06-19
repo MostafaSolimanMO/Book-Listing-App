db32608fe9d10e8b661100f0ec7363be5bb33783
Mostafa Soliman <mostafamoma.moma@gmail.com>
2019-06-20 01:28:53 +0200

    Some editing on EditText and Search button

--- app/src/main/java/daii/bookListing/MainActivity.java
+++ app/src/main/java/daii/bookListing/MainActivity.java
@@ -2,9 +2,9 @@ package daii.bookListing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
-import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    String searchKey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +23,17 @@ public class MainActivity extends AppCompatActivity {
         BooksAdapter adapter = new BooksAdapter(this, books);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
+        final EditText searchEdit = findViewById(R.id.Search_key);
+        ImageButton searchButton = findViewById(R.id.Search_Button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchKey = searchEdit.getText().toString();
+                Toast.makeText(getApplicationContext(), searchKey, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
-
 
 }
 
