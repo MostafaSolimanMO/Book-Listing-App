0540e4de0f848fe9752be45cdfc6919deeeef2aa
Mostafa Soliman <mostafamoma.moma@gmail.com>
2019-06-19 23:53:09 +0200

    Replace SearchView by EditText

--- app/src/main/java/daii/bookListing/Books.java
+++ app/src/main/java/daii/bookListing/Books.java
@@ -3,18 +3,22 @@ package daii.bookListing;
 public class Books {
     private String mTitle;
     private String mAuthor;
+    private String mImageUrl;
 
-    public Books(String title, String author) {
+    public Books(String title, String author, String imageUrl) {
         mTitle = title;
         mAuthor = author;
+        mImageUrl = imageUrl;
     }
 
     public String getmTitle() {
         return mTitle;
     }
-
     public String getmAuthor() {
         return mAuthor;
     }
 
+    public String getmImageUrl() {
+        return mImageUrl;
+    }
 }
--- app/src/main/java/daii/bookListing/MainActivity.java
+++ app/src/main/java/daii/bookListing/MainActivity.java
@@ -2,25 +2,30 @@ package daii.bookListing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<Books> books = new ArrayList<>();
-        books.add(new Books("Android Application Development", "Ryan Cohen"));
-        books.add(new Books("Android on x86", "Iggy Krajci"));
+        books.add(new Books("Android Application Development", "Ryan Cohen", "sa"));
         BooksAdapter adapter = new BooksAdapter(this, books);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
 
     }
+
+
 }
+
 
 
